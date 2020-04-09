773cc5e2bba58dc863fa5c5c9daa39f0742a786a
Julia Tadej <46266043+happyberry@users.noreply.github.com>
2020-04-09 20:55:48 +0200

    Add files via upload

--- PastGames/src/main/java/com/sample/DroolsTest.java
+++ PastGames/src/main/java/com/sample/DroolsTest.java
@@ -1,13 +1,9 @@
 package com.sample;
 
 import org.kie.api.KieServices;
-import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.logger.*;
-
-import javax.swing.JOptionPane;
-
 
 /**
  * This is a sample class to launch a rule.
@@ -16,37 +12,50 @@ public class DroolsTest {
 
     public static final void main(String[] args) {
         try {
-        	KieServices ks = KieServices.Factory.get();
-     	    KieContainer kContainer = ks.getKieClasspathContainer();
-         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-         	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
-         	
-             // go !
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
 
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
             kSession.fireAllRules();
             kLogger.close();
-        
+            
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-    
-    public static class UiHandler{
-    	
-    	
-		public static String showQuestionAndGetAnswer(String question, Object[] answers){
-    		JOptionPane questionPane = new JOptionPane();
-    		int result = questionPane.showOptionDialog(null, question, "ChooseTheGame", 
-    		JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, answers, null);
-    		return (String) answers[result];
-    	}
-    	
-		public static void showDialog(String text){
-			JOptionPane questionPane = new JOptionPane();
-			questionPane.showMessageDialog(null, text, "ChooseTheGame", JOptionPane.PLAIN_MESSAGE);
-		}	
-		
-    }
 
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
 
 }
